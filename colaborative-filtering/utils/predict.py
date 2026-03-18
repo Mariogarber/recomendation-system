6179bdabf603e1ba8807c040f399e9f0a77b5824
Mariogarber <mario.garcia.berenguer@gmail.com>
2026-03-18 17:33:02 +0100

    Add predictor class compatible with models

---
+++ colaborative-filtering/utils/predict.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+import os
+
+from model.base import BaseModel
+
+class Predictor:
+    def __init__(self, test_path: str, save_path: str, round_predictions: bool = False):
+        self.path = test_path
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        self.save_path = save_path
+        self.round_predictions = round_predictions
+
+    def load_test_from_csv(self):
+        """
+        Carga un CSV con columnas 'user', 'item' y 'prediction' y devuelve un DataFrame.
+        """
+        import pandas as pd
+        df = pd.read_csv(self.path)
+        if not all(col in df.columns for col in ["ID", "user", "item"]):
+            raise ValueError("El CSV debe contener las columnas 'ID', 'user' y 'item'.")
+        
+        ids = df["ID"].to_numpy()
+        return df[["user", "item"]], ids
+
+    def compute_predictions(self, model: BaseModel, test_df: pd.DataFrame):
+        """
+        Dado un modelo y un DataFrame con columnas 'user' e 'item', devuelve un array con las predicciones.
+        """
+        preds = []
+        for u, i in zip(test_df["user"], test_df["item"]):
+            try:
+                p = model.predict(u, i)
+            except Exception:
+                p = 7.0  # Valor por defecto en caso de error
+
+            if self.round_predictions:
+                p = round(p)
+            preds.append(p)
+        return np.asarray(preds, dtype=float)
+    
+    def predict(self, model: BaseModel):
+        test_df, ids = self.load_test_from_csv()
+        predictions = self.compute_predictions(model, test_df)
+        solution_df = pd.DataFrame({"ID": ids, "rating": predictions})
+        solution_df.to_csv(self.save_path, index=False)
+        return solution_df
